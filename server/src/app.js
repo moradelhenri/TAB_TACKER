8df90f8d9b315336db0c246d3f63397a6be5b392
Moradel Henri <moradelhenri@gmail.com>
2022-02-12 13:54:28 +0100

    client refait

--- server/src/app.js
+++ server/src/app.js
@@ -5,12 +5,12 @@ const morgan = require("morgan");
 
 const app = express();
 app.use(morgan("combine"));
-app.use(bodyParser.json());
+ app.use(bodyParser.json())
 app.use(cors());
 
-app.get("/status", (req, res) => {
+app.post("/register", (req, res) => {
   res.send({
-    message: "Hello World!",
+    message: `Hello ${req.body.email}! your user was registered! have fun!`,
   });
 });
 app.listen(process.env.PORT || 8081);
